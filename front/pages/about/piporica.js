f5a32e0b3660a1406a2bb3b19efeb7aa134480a9
piporica <49173614+piporica@users.noreply.github.com>
2023-04-30 22:37:09 +0900

    ch.0

---
+++ front/pages/about/piporica.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Aboutpiporica = () => {
+  return <div> P I P O R I C A </div>;
+};
+
+export default Aboutpiporica;
---
+++ front/pages/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayOut from "../components/AppLayout";
+
+const Profile = () => {
+  return (
+    <AppLayOut>
+      <div> 내 프로필 </div>
+    </AppLayOut>
+  );
+};
+
+export default Profile;
